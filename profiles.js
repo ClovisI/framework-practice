151aea42189b830525b69a3f34f6cf08119a130f
Cheri Allen <cherimarie@gmail.com>
2018-08-09 20:47:51 -0700

    Filtering on ice cream working

--- profiles.js
+++ profiles.js
@@ -22,7 +22,19 @@ document.addEventListener("DOMContentLoaded", function(){
   let profileListings = new Vue({
     el: '#profileListings',
     data: {
-      profiles: gators
+      profiles: gators,
+      icecreamOnly: false
+    },
+    methods: {
+      icecreamFilter: function(){
+        if(this.icecreamOnly){
+          this.profiles = gators.filter(function(element){
+            return element.likesIceCream
+          })
+        } else {
+          this.profiles = gators
+        }
+      }
     }
   })
 
